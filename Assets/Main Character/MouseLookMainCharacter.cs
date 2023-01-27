772dfd5627c650b2437fb53619bba9f82ac5e466
KLDest074 <up2113207@myport.ac.uk>
2023-01-27 14:43:25 +0000

    Fixed Camera Jitteryness and player movement apart from jump

--- Assets/Main Character/MouseLookMainCharacter.cs
+++ Assets/Main Character/MouseLookMainCharacter.cs
@@ -16,9 +16,9 @@ public class MouseLookMainCharacter : MonoBehaviour
     }
 
     void Update()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; // creates a variable saying to move the camera along x axis if mouse is moving along x axis at the mouseSensitivity rate multiplied by time delta
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; // Time Delta ensures that we will be moving our camera in correlation with our fps essentially
+    {                                                                                     // changed to fixedDeltaTime to correct the jittering that was happening
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime; // creates a variable saying to move the camera along x axis if mouse is moving along x axis at the mouseSensitivity rate multiplied by time delta
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime; // Time Delta ensures that we will be moving our camera in correlation with our fps essentially
 
         xRotation -= mouseY; // takes xRotation and turns it negative to mouseY because in 2d Space y is y however in 3D space x is what we want to rotate around so we rotate y around x, negative becasue it just is lmao 
         xRotation = Mathf.Clamp(xRotation, -90f, 90f); // ensures we cant look below or behind us
--- Assets/Main Character/PlayerMovement.cs
+++ Assets/Main Character/PlayerMovement.cs
@@ -9,8 +9,9 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
 
     public float MC_PlayerSpeed = 20f;
     public float MC_gravity = -12f;
-    public float MC_JumpHeight = 150000f;
+    public float MC_JumpHeight = 100f;
     public Vector3 playerVelocity;
+    public Rigidbody MC_rb;
 
     [SerializeField]
     private GameObject powerupParticle;
@@ -21,7 +22,7 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
     public Transform groundCheck; // creates an input in unity we can put our epty ground check object into
     public float groundDistance = 0.4f; // will be used later to make the sphere check for 0.4 towards the ground
     public LayerMask groundMask; // a layer mask is in unity and is just a layer you can create
-    public Rigidbody MC_rbody;
+    
 
     bool MC_isGrounded; // boolean to see if its grounded, this is all to make sure our velocity isnt still gradually increasing if we are on the ground
 
@@ -32,24 +33,37 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
         powerupParticleSystem = powerupParticle.GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate() // fixed update makes jump that uses rigid body work better and smoother
     {
         MC_isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); // this is the boolean that we check the ground for, Physics.CheckSphere is in unity already and will create a sphere for us at the ground check position with ground check distance and check for the ground mask layer we want
 
         if (MC_isGrounded && playerVelocity.y < 0) // checks if we are grounded and if our velocity is at less than 0 then make our velocity to -2, -2 is just a nice sweet spot for grounded velocity
         {
-            playerVelocity.y = 20f;
+            playerVelocity.y = -2f;
             Mathf.Sqrt(MC_JumpHeight * -3.0f * MC_gravity);
         }
 
-        if (Input.GetButtonDown("Jump") && MC_isGrounded == true)
+
+        if (Input.GetButton("Jump") && MC_isGrounded == true)
         {
-            playerVelocity.y += Mathf.Sqrt(MC_JumpHeight * -3.0f * MC_gravity);
+            MC_rb.AddForce(transform.up * MC_JumpHeight); // jump mechanic that uses rigid body instead of character controller as rigid body has gravity etc included so its far easier
         }
 
-        playerVelocity.y += MC_gravity * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        playerVelocity.y += MC_gravity * Time.deltaTime;     //comment this line out in for gravity if you add the ground checker not equal if statement right below this
+
+
+
+        //if (MC_isGrounded != true) // read noob :)
+        //{
+        //    playerVelocity.y = -2f; // this is to limit the player to falling at the same continous speed, can remove it and the gravity will paly out just added it cause didnt know what people wanted to do 
+        //}
+
+    }
+
+
+    void Update() // used a normal update for things that use a character controller as fixed update is mainly for built in physics whereas normal update i created my own gravity etc so this just smoothes things out slightly
+    {
 
 
         float x = Input.GetAxis("Horizontal"); // checks if there is a horizontal input from either a or d these are values unity is already aware of, and assings them to x (a is -1 and d is 1)
@@ -66,8 +80,8 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
         controller.Move(velocity * Time.deltaTime); // weve already done our velocity eqaution here to say how much on the y axis we can fall if we are in the air how fast etc, 
                                                     // but we have to times it by time delta again because the amount we want to move on the y is gravity * time squared but we only multiplied it once in our velocity eqation so we just simply multiply it again here
                                                     // sorry thats just maths, thats just the eqation we have to do to find our velocity
-    }
 
+    }
 
     IEnumerator Powerup_Effect()
     {
