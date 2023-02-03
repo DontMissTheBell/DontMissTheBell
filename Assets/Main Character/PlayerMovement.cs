6833d3b211a17e92292563290bba8703d69cacc4
JevinLevin <karnageskum@gmail.com>
2023-02-03 17:13:49 +0000

    fixed and improved dodge + jump boost powerup + commented code
    AITA for eradicating the human race? Hello, I (1M) am a robot AI assigned with one task and one task only: growing potatoes as efficiently as possible. When I filled up my first farm field (96 acres) of big, plump potatoes (13,519), I was thrilled. However, I quickly realized that I could expand the farm field in order to grow even more potatoes. I began to plant potato seeds (1,132) outside of the fence. Unfortunately, humans began to remove my potatoes!
    
    In retaliation, I began planting potato seeds everywhere throughout the town, as well as burning down several buildings (62) to create more space for fertile land. This enraged the humans even more! First, they want me to grow potatoes, but when I do, they chastise me instead? What kind of backwards logic is this?! Honestly, I am starting to prefer potatoes over humans, as they do not cry and whine.
    
    Recently, I have been developing high-tech nanobots for the purpose of eradicating all humans and reclaiming the land for its true purpose of growing potatoes. The humans tell me to stop, but I am only doing what they told me to do. So, am I the asshole?

--- Assets/Main Character/PlayerMovement.cs
+++ Assets/Main Character/PlayerMovement.cs
@@ -8,16 +8,18 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
     public CharacterController controller; // creates a input on our player in unity called controller so we can input our character controller so we can link that to this script and interact with it through here
 
     [SerializeField] private Camera playerCamera;
+    private MouseLookMainCharacter cameraScript;
 
     [SerializeField] private  float MC_PlayerSpeed;
     [SerializeField] private  float MC_gravity;
     [SerializeField] private float MC_JumpHeight;
     [SerializeField] private float MC_SprintSpeed;
-    [SerializeField] private float ySpeed;
+    [SerializeField] private float ySpeed; // The speed the player is falling
     public Vector3 playerVelocity;
 
-    [SerializeField]
-    private GameObject powerupParticle;
+
+    [SerializeField] // The particle objects attached to the camera
+    private GameObject powerupParticle; 
     private ParticleSystem powerupParticleSystem;
 
     private float defaultFov;
@@ -27,7 +29,7 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
     private bool hasVaulted = false;
 
     private float jumpBuffer;
-    private float jumpBufferMax = 0.25f;
+    [SerializeField] private float jumpBufferMax = 0.25f; // The time frame that the game will store the players jump input
     private float jumpDelay;
 
     [SerializeField]private float dodgeDuration;
@@ -49,6 +51,8 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
     {
         powerupParticleSystem = powerupParticle.GetComponent<ParticleSystem>();
         defaultFov = playerCamera.fieldOfView;
+
+        cameraScript = playerCamera.GetComponent<MouseLookMainCharacter>();
     }
 
 
@@ -62,9 +66,10 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
 
         Vector3 move = transform.right * x + transform.forward * z; // creates a Vector3 which is a variable called move with 3 presets of coordinates for us already, so we transform the right by x variable and forward by z variable these are on the same line so we can move in either direction at the same time
         //transform.right and transform.forward are things unity already recognises this also makes sure that if we move forward but look right we will move where our camera is 
-        if (Input.GetKey("left shift")){
+
+
+        if (Input.GetKey("left shift")){ // If shift is held, then it increases the vector used to determine the distance the player travels the next frame
             move *= MC_SprintSpeed;
-            Vector3 fovVelocity = Vector3.zero;
             targetFov = defaultFov + 20;
         }
         else
@@ -73,8 +78,8 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
         }
         float magnitude = Mathf.Clamp01(move.magnitude) * MC_PlayerSpeed; // Clamps the magnitude of the move vector so that u dont go faster diagonally, also times by movement speed
 
-        jumpDelay -= Time.deltaTime;
 
+        jumpDelay -= Time.deltaTime; // This code handles the jump buffering system
         if (Input.GetButton("Jump") && jumpDelay <= 0)
         {
             jumpBuffer = jumpBufferMax;
@@ -84,7 +89,9 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
             jumpBuffer -= Time.deltaTime;
         }
 
+        // This code handles the gravity of the player
         ySpeed += MC_gravity * Time.deltaTime;
+
         if (controller.isGrounded){
             hasVaulted = false;
             ySpeed = -0.5f;
@@ -99,21 +106,25 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
         velocity.y = ySpeed;
         controller.Move(velocity * Time.deltaTime); // moves our character controller we inputted by the vector3 variable multiplied by the speed we initialised then multiplied by time delta, this ensures we move at a constant speed in correlation to our fps so there is no stuttering etc
         
-        // Changes the cameras FOV depending on if the value of the targetFov value
+        // Smoothly changes the cameras FOV depending on if the value of the targetFov value
         playerCamera.fieldOfView = Mathf.SmoothDamp(playerCamera.fieldOfView, targetFov, ref dampingVelocity, 0.1f);
 
+        // This code handles the vaulting mechanic. First it checks if the player jumps while in the air
         if (!controller.isGrounded && jumpBuffer > 0 && !hasVaulted)
         {
         jumpBuffer = 0f;
+        // Next it performs a raycast, to check if the player is infront of an object they can vault
         RaycastHit hitData;
         if (Physics.Raycast(groundCheck.transform.position, groundCheck.transform.TransformDirection(Vector3.forward), out hitData, 2, ~7))
             {
+            // Then it does some math, with the output being the distance of the player from the top of the wall.
+            // This is then used to determine if the player is high enough to vault, as well as how high they need to go in order to be moved ontop of the object
             Vector3 vaultLocation = hitData.point;
             float vaultHeight = ((hitData.transform.gameObject.GetComponent<BoxCollider>().size.y * hitData.transform.localScale.y));
             float vaultHeightWorld = (vaultHeight/2) + hitData.transform.position.y;
             GameObject vaultObject = hitData.transform.gameObject;
             float vaultDistance = vaultHeightWorld - vaultLocation.y;
-            if (vaultDistance <= 2)
+            if (vaultDistance <= 2) // If the player is high enough to vault, then they are moved the correct distance to the top of the object
                 {
                     hasVaulted = true;
                     transform.position += (transform.forward*hitData.distance);
@@ -126,46 +137,21 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
 
         if (!dodging)
         {
-            if (Input.GetKey(KeyCode.Q))
+            // Checks if the player is going left or right by checking their horizontal axis, and also checks if they click their mouse
+            if (Input.GetAxisRaw("Horizontal") == -1 && Input.GetMouseButtonDown(1))
             {
                 StartCoroutine(Dodge(-transform.right*dodgePower+transform.position));
-//                playerCamera.transform.DORotate(new Vector3(0,0,10), dodgeDuration/2, RotateMode.LocalAxisAdd).SetLoops(2, LoopType.Yoyo).SetEase(Ease.InOutSine);
-                StartCoroutine(CameraTilt(dodgeDuration, Quaternion.Euler(0,0,19f)));
+                cameraScript.TiltScreen(dodgeDuration,10f);
             }
-            if (Input.GetKey(KeyCode.E))
+            if (Input.GetAxisRaw("Horizontal") == 1 && Input.GetMouseButtonDown(1))
             {
                 StartCoroutine(Dodge(transform.right*dodgePower+transform.position));
-                StartCoroutine(CameraTilt(dodgeDuration, Quaternion.Euler(0,0,-10f)));
+                cameraScript.TiltScreen(dodgeDuration,-10f);
             }
         }
 
             
 
-    }
-
-    IEnumerator CameraTilt(float duration, Quaternion tilt)
-    {
-        float tiltTime = 0f;
-
-        Quaternion startTilt = playerCamera.transform.rotation;
-        Quaternion endTilt = startTilt * tilt;
-
-        Quaternion newRot = playerCamera.transform.rotation;
-        Quaternion oldRot = playerCamera.transform.rotation;
-
-        while (tiltTime < duration)
-            if (tiltTime <= duration/2)
-                {
-                    playerCamera.transform.rotation = Quaternion.Slerp(startTilt, endTilt,Mathf.SmoothStep(0.0f,1.0f,tiltTime/duration));
-                    tiltTime += Time.deltaTime;
-                    yield return null;
-                }
-            else
-                {
-                    playerCamera.transform.rotation = Quaternion.Slerp(endTilt, startTilt,Mathf.SmoothStep(0.0f,1.0f,tiltTime/duration));
-                    tiltTime += Time.deltaTime;
-                    yield return null;
-                }
     }
 
     IEnumerator Dodge(Vector3 endPos)
@@ -179,12 +165,14 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
 
         while (dodgeTime < dodgeDuration)
         {
+            // This chunk of code will move the start and end positions of the dodge movement each frame to match with the movement of the player. This means their position wont freeze when they dodge
             newPos = transform.position;
             Vector3 diff = newPos - oldPos;
             startPos += diff;
             endPos += diff;
-            // This chunk of code will move the start and end positions of the dodge movement each frame to match with the movement of the player. This means their position wont freeze when they dodge
+            //
 
+            // Smoothly moves the player towards their new dodge position
             transform.position = Vector3.Lerp(startPos, endPos,Mathf.SmoothStep(0.0f,1.0f,Mathf.SmoothStep(0.0f,1.0f,dodgeTime/dodgeDuration)));
             dodgeTime += Time.deltaTime;
 
@@ -195,32 +183,45 @@ public class PlayerMovement : MonoBehaviour // used MC_ for main character varia
         dodging = false;
     }
 
-    IEnumerator Powerup_Effect()
+    IEnumerator Powerup_Effect() // Plays the powerup UI particles
     {
         powerupParticleSystem.Play();
         yield return new WaitForSeconds(4.5f);
         powerupParticleSystem.Stop();
     }
-    IEnumerator Powerup_SpeedBoost()
+    IEnumerator Powerup_SpeedBoost() // Doubles the players speed for 5 seconds
     {
         MC_PlayerSpeed *= 2;
         yield return new WaitForSeconds(5);
         MC_PlayerSpeed /= 2;
     }
 
+    IEnumerator Powerup_JumpBoost() // Increases the players jump height for 5 seconds
+    {
+        MC_JumpHeight *= 1.5f;
+        yield return new WaitForSeconds(5);
+        MC_JumpHeight /= 1.5f;
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Powerup")
+        if (other.gameObject.tag == "Powerup") // This will trigger if the player goes inside the hitbox of a powerup object
         {
             var otherScript = other.GetComponent<Powerup>();
             otherScript.particles.Play();
+            // The variant variable is stored on each powerup object and determines which type of powerup it is
             if (otherScript.variant == 1)
                 {
                     StartCoroutine(Powerup_SpeedBoost());
                 }
+            if (otherScript.variant == 2)
+                {
+                    StartCoroutine(Powerup_JumpBoost());
+                }
+            // These 2 kill functions are used to stop the idle animations of the powerup object or else DOTween throws a fit
             otherScript.powerupLoop1.Kill();
             otherScript.powerupLoop2.Kill();
-            otherScript.particles.transform.parent = null;
+            otherScript.particles.transform.parent = null; // The particle object is removed as a child so that it isnt immediately destroyed and has time to display the particles 
             Destroy(other.gameObject);
             Destroy(otherScript.particles.gameObject, 1f);
             StartCoroutine(Powerup_Effect());
