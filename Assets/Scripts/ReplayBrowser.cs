46ec835c519fca00da5095050d017a47dfb27fb5
quagsirus <quagsirus@catpowered.net>
2023-03-20 19:46:07 +0000

    more cleanup

--- Assets/Scripts/ReplayBrowser.cs
+++ Assets/Scripts/ReplayBrowser.cs
@@ -8,18 +8,25 @@ using UnityEngine.UI;
 
 public class ReplayBrowser : MonoBehaviour
 {
-    private delegate void RawEntries(string rawEntries, int page, int pageSize);
-    private event RawEntries RawEntriesDownloaded;
     private readonly List<GameObject> leaderboardEntries = new();
+
+    public void Start()
+    {
+        RawEntriesDownloaded += ProcessLeaderboardEntries;
+        StartCoroutine(DownloadEntries());
+        leaderboardEntries.Add(transform.GetChild(0).gameObject);
+    }
+
+    private event RawEntries RawEntriesDownloaded;
 
     private void ProcessLeaderboardEntries(string serverResponse, int page, int pageSize)
     {
         // Deserialize LeaderboardSection object
-        if (serverResponse == "null") { return; }
+        if (serverResponse == "null") return;
         var leaderboardSection = JsonUtility.FromJson<LeaderboardSection>(serverResponse);
         // Abort if json did not contain a valid LeaderboardSection object
         if (leaderboardSection.entries == null) return;
-        
+
         // Create a panel for each LeaderboardEntry object in leaderboardSection.entries
         foreach (var leaderboardEntry in leaderboardSection.entries)
         {
@@ -35,15 +42,8 @@ public class ReplayBrowser : MonoBehaviour
 
             // Assign function call to Watch button
             var button = currentEntry.GetComponentInChildren<Button>();
-            button.onClick.AddListener(delegate {StartReplay(leaderboardEntry.ghostId, leaderboardEntry.levelId); });
+            button.onClick.AddListener(delegate { StartReplay(leaderboardEntry.ghostId, leaderboardEntry.levelId); });
         }
-    }
-
-    public void Start()
-    {
-        RawEntriesDownloaded += ProcessLeaderboardEntries;
-        StartCoroutine(DownloadEntries());
-        leaderboardEntries.Add(transform.GetChild(0).gameObject);
     }
 
     private IEnumerator DownloadEntries(int page = 1, int pageSize = 10)
@@ -52,22 +52,20 @@ public class ReplayBrowser : MonoBehaviour
         yield return www.SendWebRequest();
 
         if (www.result != UnityWebRequest.Result.Success)
-        {
             Debug.Log($"{www.error} - {www.downloadHandler.text}");
-        }
         else
-        {
             //Debug.Log($"Successfully downloaded leaderboard page {page}");
             RawEntriesDownloaded?.Invoke(www.downloadHandler.text, page, pageSize);
-        }
     }
 
     private static void StartReplay(string ghostId, int levelId)
     {
         Globals.Instance.replayToStart = ghostId;
-        
+
         Globals.Instance.StartCoroutine(Globals.Instance.TriggerLoadingScreen(sceneId: levelId));
     }
+
+    private delegate void RawEntries(string rawEntries, int page, int pageSize);
 }
 
 [Serializable]
