bfec693394eb6f643dba53157c68eeb9bde989a5
KLDest074 <up2113207@myport.ac.uk>
2023-04-11 14:16:52 +0100

    Start of HUD

---
+++ Assets/Scripts/HUDlayout.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDlayout : MonoBehaviour
+{
+    
+    public GameObject HomeworkIcon;
+    public GameObject SpeedBoostIcon;
+    public GameObject JumpBoostIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HomeworkIcon.SetActive(false);
+        //SpeedBoostIcon.SetActive(false);
+        //JumpBoostIcon.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //public void SetEnabled(bool HomeworkIcon)
+    //{
+    //    if (homeworkCompleted)
+    //    {
+    //        HomeworkIcon.SetActive(true);
+    //    }
+    //}
+}
