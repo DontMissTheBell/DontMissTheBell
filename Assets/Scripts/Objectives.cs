24b5b211914a24b73ee3a77c03fa7e884356c552
Cambob03 <cameron.best@outlook.com>
2023-04-13 19:03:43 +0100

    Homework spin/destroy/prefab

--- Assets/Scripts/Objectives.cs
+++ Assets/Scripts/Objectives.cs
@@ -1,8 +1,20 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Objectives : MonoBehaviour
 {
     public bool homeworkCompleted;
+
+    public Tweener tweener1;
+    public Tweener tweener2;
+
+    private void Start()
+    {
+        tweener1 = transform.DOMove(transform.position + Vector3.up / 2, 1.5f).SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+        tweener2 = transform.DORotate(new Vector3(0, 360, 0), 3, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
