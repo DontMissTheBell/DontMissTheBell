22b5b6afea4fca80ca3865f7ed1f3a02bccfd88e
JevinLevin <karnageskum@gmail.com>
2023-05-12 14:03:01 +0100

    added press esc text

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,7 +4,18 @@ using DG.Tweening;
 
 public class PauseMenu : MonoBehaviour
 {
+
+    [SerializeField] GameObject pressEscText;
+
     // Update is called once per frame
+
+    private void Awake()
+    {
+        if (!Globals.HasUsedPause)
+        {
+            pressEscText.SetActive(true);
+        }
+    }
     private void Update()
     {
         // If Pause button pressed
@@ -14,6 +25,11 @@ public class PauseMenu : MonoBehaviour
             Globals.Instance.gamePaused = 0.0f == Time.timeScale;
             gameObject.GetComponentInChildren<Canvas>(true).enabled = 0.0f == Time.timeScale;
             Cursor.lockState = Globals.Instance.gamePaused ? CursorLockMode.None : CursorLockMode.Locked;
+
+            pressEscText.SetActive(false);
+            
+
+            Globals.HasUsedPause = true;
         }
             
     }
